a194bd557f2b391f0b6862823047022d670fa59b
y423610m <87542096+y423610m@users.noreply.github.com>
2022-04-17 07:49:33 +0900

    Update and rename E.cpp to bfs_01.cpp

--- bfs_01.cpp
+++ bfs_01.cpp
@@ -59,33 +59,6 @@ istream &operator>>(istream &is, vector< T > &v) {
     return is;
 }
 
-/*
-std::ostream&
-operator<<( std::ostream& dest, __int128_t value )
-{
-    std::ostream::sentry s( dest );
-    if ( s ) {
-        __uint128_t tmp = value < 0 ? -value : value;
-        char buffer[ 128 ];
-        char* d = std::end( buffer );
-        do
-        {
-            -- d;
-            *d = "0123456789"[ tmp % 10 ];
-            tmp /= 10;
-        } while ( tmp != 0 );
-        if ( value < 0 ) {
-            -- d;
-            *d = '-';
-        }
-        int len = std::end( buffer ) - d;
-        if ( dest.rdbuf()->sputn( d, len ) != len ) {
-            dest.setstate( std::ios_base::badbit );
-        }
-    }
-    return dest;
-}
-*/
 
 template <class T, class U> void chmin(T& t, const U& u) {if (t > u) t = u;}
 template <class T, class U> void chmax(T& t, const U& u) {if (t < u) t = u;}
