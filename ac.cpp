919a83a9a848c0697ec36e68b1ee90f2821fe49b
y423610m <87542096+y423610m@users.noreply.github.com>
2022-04-23 12:45:05 +0900

    added sum functions

--- ac.cpp
+++ ac.cpp
@@ -6,6 +6,9 @@ using namespace atcoder;
 const int MOD = 998244353;
 //const int MOD = 1000000007;
 
+const int INF = (1<<30)-1;
+const long long LINF = (1LL<<62)-1;
+
 #define rep(i, n) for (int i = 0; i < (int)(n); i++)
 #define repi(i, a, n) for (int i = a; i < (int)(n); i++)
 #define repll(i, n) for (ll i = 0; i < (ll)(n); i++)
@@ -20,48 +23,35 @@ const int MOD = 998244353;
 #define END(a) {PL(a) return;}
 #define FI first
 #define SE second
+#define SORT(a) sort(a.begin(), a.end());
+#define REVERSE(a) reverse(a.begin(), a.end());
+#define ERASE(a) a.erase(unique(a.begin(), a.end()), a.end());
 
 using ll = long long;
 using ull = unsigned long long;
-using VI = vector<int>;
 using P = pair<int, int>;
 using mint = atcoder::static_modint<MOD>;
 
-const int INF = (1<<30)-1;
-const ll LINF = (1LL<<62)-1;
-
-template< typename T >
-ostream &operator<<(ostream &os, const vector< T > &v) {
-    for(int i = 0; i < (int) v.size(); i++) {
-    	os << v[i] << (i + 1 != (int) v.size() ? " " : "");
-    }
-    return os;
-}
-
-ostream &operator<<(ostream &os, const mint x) {
-    os<<x.val();
-    return os;
-}
-
-template< typename T >
-istream &operator>>(istream &is, vector< T > &v) {
-    for(T &in : v) is >> in;
-    return is;
-}
-
+template< typename T >ostream &operator<<(ostream &os, const vector< T > &v) {for(int i = 0; i < (int) v.size(); i++) {os << v[i] << (i + 1 != (int) v.size() ? " " : "");}return os;}
+template< typename T >istream &operator>>(istream &is, vector< T > &v) {for(T &in : v) is >> in;return is;}
+ostream &operator<<(ostream &os, const mint x) {os<<x.val();return os;}
 
 template <class T, class U> void chmin(T& t, const U& u) {if (t > u) t = u;}
 template <class T, class U> void chmax(T& t, const U& u) {if (t < u) t = u;}
 
 template <class T> using V = vector<T>;
 
+vector<string> SEPARATE(string s_in, string split=","){ vector<string> vs;	string tmp; for(int i=0;i<s_in.size();i++){	bool found = false;		for(int j=0;j<split.size();j++){ if(s_in[i]==split[j]){	if(tmp!=""){vs.push_back(tmp);} tmp = ""; found = true; break; }}	if(!found) tmp+=s_in[i];	} 	if(tmp!="") vs.push_back(tmp);	return vs;}
+ll POW(ll a, ll b){	ll ret = 1;	ll tmp = a;	while(b){if(b&1){ret *= tmp;} b /= 2; tmp *= tmp;}	return ret;}
+vector<ll> DIGIT(ll a, int b=2){vector<ll> ret;while(a){ret.push_back(a%b); a/=b; }reverse(ret.begin(), ret.end());return ret;}
+
 int DX[4] = {1, 0, -1, 0};
 int DY[4] = {0, 1, 0, -1};
 
 
 void solve() {
-	
-    
+
+
 
 	return;
 }
