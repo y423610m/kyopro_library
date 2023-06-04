f00772cbb75266731baa0475afe3ef373bee6339
y423610m <y423610m@icloud.com>
2023-06-04 22:53:36 +0900

    added core files

---
+++ core/RemoveHash.py
@@ -0,0 +1,11 @@
+lines = []
+f = open("SUB.cpp")
+for line in f:
+    if not "# " in line:
+        lines.append(line)
+f.close()
+
+f = open("SUB.cpp","w")
+for line in lines:
+    f.write(line)
+f.close()
