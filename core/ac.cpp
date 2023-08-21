94cca08fe75cf2badef9f37cc7514c4d74001023
y423610m <y423610m@icloud.com>
2023-08-21 23:27:43 +0900

    add mint space

--- core/ac.cpp
+++ core/ac.cpp
@@ -13,6 +13,10 @@ constexpr ll MOD = 998'244'353;
 // #define _GLIBCXX_DEQUE_BUF_SIZE 512
 // #pragma comment(linker, "/stack:1000000000")
 
+
+//mint
+
+
 // int:[-2'147'483'648 : 2'147'483'647]
 // ll:[-9'223'372'036'854'775'808 : 9'223'372'036'854'775'807]
 constexpr ll INF = (1LL<<30)-1;
