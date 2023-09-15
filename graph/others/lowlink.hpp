77de24a209ef81c3a83eb1c2172d0ba2cfc51766
y423610m <y423610m@icloud.com>
2023-09-16 04:18:12 +0900

    add low-link

---
+++ graph/others/lowlink.hpp
@@ -0,0 +1,48 @@
+#pragma once
+
+#include "graph/graph_template.hpp"
+
+template<typename T>
+struct LowLink {
+    const Graph<T> &G;
+    vector<int> used, ord, low;
+    vector<int> aps;  // articulation points
+    vector<pair<int, int>> bridges;
+    vector<ll> bridgeIds;
+    LowLink(const Graph<T> &G_) : G(G_) {
+        used.assign(G.size(), 0);
+        ord.assign(G.size(), 0);
+        low.assign(G.size(), 0);
+        int k = 0;
+        for (int i = 0; i < (int)G.size(); i++) {
+            if (!used[i]) k = dfs(i, k, -1);
+        }
+        sort(aps.begin(), aps.end()); // 必要ならソートする
+        sort(bridges.begin(), bridges.end()); // 必要ならソートする
+        sort(ALL(bridgeIds));
+    }
+    int dfs(int id, int k, int par) { // id:探索中の頂点, k:dfsで何番目に探索するか, par:idの親
+        used[id] = true;
+        ord[id] = k++;
+        low[id] = ord[id];
+        bool is_aps = false;
+        int count = 0; // 子の数
+        for (auto &e : G[id]) {
+            if (!used[e.to]) {
+                count++;
+                k = dfs(e.to, k, id);
+                low[id] = min(low[id], low[e.to]);
+                if (par != -1 && ord[id] <= low[e.to]) is_aps = true; 
+                if (ord[id] < low[e.to]){
+                  bridges.emplace_back(min(id, e.to), max(id, e.to)); // 条件を満たすので橋
+                  bridgeIds.push_back(e.id);  
+                }
+            } else if (e.to != par) { // eが後退辺の時
+                low[id] = min(low[id], ord[e.to]);
+            }
+        }
+        if (par == -1 && count >= 2) is_aps = true; 
+        if (is_aps) aps.push_back(id);
+        return k;
+    }
+};
