605e25c9fe728cd1e8589dd173949e616fb45109
y423610m <y423610m@icloud.com>
2024-01-14 02:03:35 +0900

    unify union find libs

--- structure/union_find/union_find.hpp
+++ structure/union_find/union_find.hpp
@@ -1,6 +1,8 @@
 #pragma once
 
-template<typename KeyT, typename ParentT, typename SizeT, typename ChildT>
+using ll = long long;
+
+template<typename KeyT, typename ParentT, typename SizeT, typename ChildT, typename WeightT>
 struct UnionFindBase
 {
 
@@ -8,6 +10,7 @@ struct UnionFindBase
    ParentT parent;//unordered_map<KeyT, KeyT> or vector<int>
    SizeT sz;//unordered_map<KeyT, int> or vetor<int>
    ChildT child;//unordered_map<KeyT, vector<KeyT>> or vector<vector<int>>
+   WeightT diffWeight;
 
    UnionFindBase(int n) : n(n)
    {
@@ -17,39 +20,53 @@ struct UnionFindBase
       return leader(a) == leader(b);
    }
 
-   bool merge(KeyT a, KeyT b){
+   bool merge(KeyT a, KeyT b, ll w = 0){
       auto MergeF = [&](KeyT x, KeyT y)->bool {return sz[x] >= sz[y];};
       auto UpdateF = [&](KeyT x, KeyT y)->void {};
-      return mergeMU(a, b, MergeF, UpdateF);
+      return mergeMU(a, b, MergeF, UpdateF, w);
    }
 
-   bool mergeM(KeyT a, KeyT b, std::function<bool(KeyT, KeyT)> MergeF){
+   bool mergeM(KeyT a, KeyT b, std::function<bool(KeyT, KeyT)> MergeF, ll w = 0){
       auto UpdateF = [&](KeyT x, KeyT y)->void {};
-      return mergeMU(a, b, MergeF, UpdateF);
+      return mergeMU(a, b, MergeF, UpdateF, w);
    }
 
-   bool mergeU(KeyT a, KeyT b, std::function<void(KeyT, KeyT)> UpdateF){
+   bool mergeU(KeyT a, KeyT b, std::function<void(KeyT, KeyT)> UpdateF, ll w = 0){
       auto MergeF = [&](KeyT x, KeyT y)->bool {return sz[x] >= sz[y];};
-      return mergeMU(a, b, MergeF, UpdateF);
+      return mergeMU(a, b, MergeF, UpdateF, w);
    }
 
    // template <typename MergeT, typename UpdateT>
    // bool mergeMU(KeyT a, KeyT b, MergeT MergeF, UpdateT UpdateF){
-   bool mergeMU(KeyT a, KeyT b, std::function<bool(KeyT, KeyT)> MergeF, std::function<void(KeyT, KeyT)> UpdateF){
+   bool mergeMU(KeyT a, KeyT b, std::function<bool(KeyT, KeyT)> MergeF, std::function<void(KeyT, KeyT)> UpdateF, ll w = 0){
+      w += weight(a);
+      w -= weight(b);
       KeyT x = leader(a);
       KeyT y = leader(b);
       if(x==y) return false;
-      if (!MergeF(x, y)) swap(x, y);
+      if (!MergeF(x, y)) swap(x, y), w=-w;
       sz[x] += sz[y];
       parent[y] = x;
       child[x].emplace_back(y);
       UpdateF(x, y);
+      diffWeight[y] = w;
       return true;
    }
 
    KeyT leader(KeyT x){
       if(parent[x]==x) return x;
-      return parent[x] = leader(parent[x]);
+      KeyT l = leader(parent[x]);// Need to call this before updating weight.
+      diffWeight[x] += diffWeight[parent[x]];
+      return parent[x] = l;
+   }
+
+   ll diff(KeyT x, KeyT y) {
+      return weight(y) - weight(x);
+   }
+
+   ll weight(KeyT x) {
+      leader(x);
+      return diffWeight[x];
    }
 
    int size(KeyT x) {
@@ -74,7 +91,7 @@ struct UnionFindBase
    }
 };
 
-struct UnionFind : public UnionFindBase<int, vector<int>, vector<int>, vector<vector<int>> >
+struct UnionFind : public UnionFindBase<int, vector<int>, vector<int>, vector<vector<int>>, vector<ll> >
 {
    UnionFind(int n) : UnionFindBase(n)
    {
@@ -82,6 +99,7 @@ struct UnionFind : public UnionFindBase<int, vector<int>, vector<int>, vector<ve
       sz.resize(n, 1);
       for(int i=0;i<n;i++) parent[i] = i;
       child.resize(n);
+      diffWeight.resize(n);
    }
 
    vector<vector<int>> groups(){
@@ -94,10 +112,10 @@ struct UnionFind : public UnionFindBase<int, vector<int>, vector<int>, vector<ve
 };
 
 template <typename KeyT>
-struct UnionFindUM : public UnionFindBase<KeyT, unordered_map<KeyT, KeyT>, unordered_map<KeyT, int>, unordered_map<KeyT, vector<KeyT>> >
+struct UnionFindUM : public UnionFindBase<KeyT, unordered_map<KeyT, KeyT>, unordered_map<KeyT, int>, unordered_map<KeyT, vector<KeyT>>, unordered_map<KeyT, ll> >
 {
 
-   using base = UnionFindBase<KeyT, std::unordered_map<KeyT, KeyT>, std::unordered_map<KeyT, int>, unordered_map<KeyT, vector<KeyT>> >;
+   using base = UnionFindBase<KeyT, std::unordered_map<KeyT, KeyT>, std::unordered_map<KeyT, int>, unordered_map<KeyT, vector<KeyT>>, unordered_map<KeyT, ll>>;
    using base::UnionFindBase;
    // using UnionFindBase<KeyT, std::unordered_map<KeyT, KeyT>, std::unordered_map<KeyT, int>>::UnionFindBase;
 
@@ -110,6 +128,7 @@ struct UnionFindUM : public UnionFindBase<KeyT, unordered_map<KeyT, KeyT>, unord
          this->n++;
          this->parent[x] = x;
          this->sz[x] = 1;
+         this->diffWeight[x] = 0;
       }
    }
 
@@ -119,28 +138,28 @@ struct UnionFindUM : public UnionFindBase<KeyT, unordered_map<KeyT, KeyT>, unord
       return base::leader(a) == base::leader(b);
    }
 
-   bool merge(KeyT a, KeyT b){
+   bool merge(KeyT a, KeyT b, ll w = 0){
       _EnsureExistance(a);
       _EnsureExistance(b);
-      return base::merge(a, b);
+      return base::merge(a, b, w);
    }
 
-   bool mergeM(KeyT a, KeyT b, std::function<bool(KeyT, KeyT)> MergeF){
+   bool mergeM(KeyT a, KeyT b, std::function<bool(KeyT, KeyT)> MergeF, ll w = 0){
       _EnsureExistance(a);
       _EnsureExistance(b);
-      return base::mergeM(a, b, MergeF);
+      return base::mergeM(a, b, MergeF, w);
    }
 
-   bool mergeU(KeyT a, KeyT b, std::function<void(KeyT, KeyT)> UpdateF){
+   bool mergeU(KeyT a, KeyT b, std::function<void(KeyT, KeyT)> UpdateF, ll w = 0){
       _EnsureExistance(a);
       _EnsureExistance(b);
-      return base::mergeU(a, b, UpdateF);
+      return base::mergeU(a, b, UpdateF, w);
    }
 
-   bool mergeMU(KeyT a, KeyT b, std::function<bool(KeyT, KeyT)> MergeF, std::function<void(KeyT, KeyT)> UpdateF){
+   bool mergeMU(KeyT a, KeyT b, std::function<bool(KeyT, KeyT)> MergeF, std::function<void(KeyT, KeyT)> UpdateF, ll w = 0){
       _EnsureExistance(a);
       _EnsureExistance(b);
-      return base::mergeMU(a, b, MergeF, UpdateF);
+      return base::mergeMU(a, b, MergeF, UpdateF, w);
    }
 
    KeyT leader(KeyT x){
@@ -151,6 +170,17 @@ struct UnionFindUM : public UnionFindBase<KeyT, unordered_map<KeyT, KeyT>, unord
    int size(KeyT x) {
       _EnsureExistance(x);
       return base::size(x);
+   }
+
+   ll diff(KeyT x, KeyT y) {
+      _EnsureExistance(x);
+      _EnsureExistance(y);
+      return base::diff(x, y);
+   }
+
+   ll weight(KeyT x){
+      _EnsureExistance(x);
+      return base::weight(x); 
    }
 
    vector<KeyT> group(KeyT a){
