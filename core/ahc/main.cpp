852fdd776d52dd7cc6d2e9bad6d4e7ca013819df
y423610m <y423610m@icloud.com>
2024-05-19 04:03:26 +0900

    update ahc

---
+++ core/ahc/main.cpp
@@ -0,0 +1,22 @@
+#include "solver.hpp"
+
+#define endl "\n"
+
+
+int main() {
+   std::cin.tie(nullptr);
+   std::ios_base::sync_with_stdio(false);
+   std::cout << std::fixed << std::setprecision(15);//小数点以下
+   // std::cout << std::setbase(16);//8進数表示.8,10,16のみ
+   // stoll(s,nullptr,base);
+   // int TT = 1;
+   // //cin>>TT;
+   // for(int tt = 0; tt<TT; tt++) solve();
+
+   Solver solver;
+   StatePtr state = solver.solve();
+
+   state->print();
+
+   return 0;
+}
