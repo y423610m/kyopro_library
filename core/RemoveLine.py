4fc031c5c87bf91b59fc8da3907425da035fafef
YoshitoMori <y423610m@icloud.com>
2026-02-15 21:45:26 +0900

    update task

--- core/RemoveLine.py
+++ core/RemoveLine.py
@@ -1,11 +1,21 @@
-lines = []
-f = open("SUB.cpp")
-for line in f:
-    if not "#line" in line:
-        lines.append(line)
-f.close()
+import sys
 
-f = open("SUB.cpp","w")
-for line in lines:
-    f.write(line)
-f.close()
+def main(filename):
+    lines = []
+    f = open(filename)
+    for line in f:
+        if not "#line" in line:
+            lines.append(line)
+    f.close()
+    
+    f = open(filename,"w")
+    for line in lines:
+        f.write(line)
+    f.close()
+
+if __name__ == '__main__':
+    if len(sys.argv) > 1:
+        filename = sys.argv[1]
+    else:
+        filename = "SUB.cpp"
+    main()
